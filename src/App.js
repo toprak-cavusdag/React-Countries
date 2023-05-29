37fe90ae373dd1cb79cf8923c6978319e8fc0b20
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-05-29 21:43:38 +0300

    folders done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { Routes as Router, Route } from 'react-router';
-
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import CountryDetail from './pages/country-detail/CountryDetail';
 function App() {
   return (
     <main className='App'>
       <Router>
-        <Route path='/' element={<Header />}></Route>
+        <Route path='/' element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path='/:code' element={<CountryDetail />} />
+        </Route>
       </Router>
     </main>
   );
